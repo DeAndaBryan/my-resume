4ec5fcb3b6fb7c2874bb2d7fe32e24e05926d5be
Bryan De Anda <bryandeanda2012@gmail.com>
2024-10-11 00:11:39 -0400

    Reverted some settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
 // src/App.jsx
 import React from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home"; // Ensure this import is correct
 import ProjectDetails from "./components/ProjectDetails"; // Detailed project page
 
 function App() {
   return (
     <Router basename="/my-resume">
-      {" "}
-      {/* Set the basename to your repository name */}
       <Routes>
-        {/* Home page (project cards) */}
         <Route path="/" element={<Home />} />
 
-        {/* Project details page */}
         <Route path="/project/:id" element={<ProjectDetails />} />
       </Routes>
     </Router>
